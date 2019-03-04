14f23213544310ecbbd27b3ecfba895099ac18d5
Lucas Simas <simas.lucas@hotmail.com>
2019-03-04 11:11:29 -0300

    Update NoElementException.java

--- src/main/java/com/github/lucassklp/jinq/exceptions/NoElementException.java
+++ src/main/java/com/github/lucassklp/jinq/exceptions/NoElementException.java
@@ -1,5 +1,7 @@
 package com.github.lucassklp.jinq.exceptions;
 
 public class NoElementException extends Exception {
-
+    public NoElementException(){
+        super("No element was found in list.");
+    }
 }
